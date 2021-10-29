7d9ab06acda236cd3d28a9dd790e273669bcf083
krasivo-exe <nicho.ivanov@gmail.com>
2021-10-29 01:42:56 -0700

    Made the background appear :)
    Nothing else works tho

--- dcui/src/Components/Battle.js
+++ dcui/src/Components/Battle.js
@@ -1,13 +1,20 @@
 import React, {useState, useEffect, Fragment} from 'react';
 import forest from './Sprites/Backgrounds/Forest.png';
+import color from "color";
 export default function Battle(props)
 {
     return <Fragment>
         {
+
             <div style={{
-                backgroundImage: forest`url(${forest})`
-            }}>
-                Battle Time :)
+                backgroundImage: `url(${forest})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                height:'540px',
+                width:'1250px',
+                }}>
+
             </div>
         }
     </Fragment>
