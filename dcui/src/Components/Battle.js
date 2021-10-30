7061160e98b14a7bc9abc1d00a8a35867bcde6d0
ryanmcalpine <57776028+ryanmcalpine@users.noreply.github.com>
2021-10-29 17:49:43 -0700

    Render sprites on screen
    Brought in the test assets and got the player character and enemy character to appear on screen.

--- dcui/src/Components/Battle.js
+++ dcui/src/Components/Battle.js
@@ -1,11 +1,18 @@
 import React, {useState, useEffect, Fragment} from 'react';
-import forest from './Sprites/Backgrounds/Forest.png';
+import Actor from "./Actor";
+import forest from './sprites/backgrounds/Forest.png';
+import testFighter from './sprites/player/test-fighter.png';
+import testSlime from './sprites/enemy/test-slime.png';
 import color from "color";
+
 export default function Battle(props)
 {
+    const spriteData = {
+        w: 160,
+        h: 200
+    }
     return <Fragment>
         {
-
             <div style={{
                 backgroundImage: `url(${forest})`,
                 backgroundPosition: 'center',
@@ -14,7 +21,8 @@ export default function Battle(props)
                 height:'540px',
                 width:'1250px',
                 }}>
-
+                <Actor sprite={testFighter} data={spriteData} />
+                <Actor sprite={testSlime} data={spriteData} />
             </div>
         }
     </Fragment>
