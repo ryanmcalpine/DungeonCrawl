883533ea162a92e4855e5b646b0436ca389b864c
krasivo-exe <nicho.ivanov@gmail.com>
2021-10-30 00:24:38 -0700

    Continuing work on endgame
    Tried some new stuff, added conditionals to end the game properly, main issue is useStates not being updated

--- dcui/src/Components/Battle.js
+++ dcui/src/Components/Battle.js
@@ -26,7 +26,8 @@ export default function Battle(props)
 
     async function handleAttack1()
     {
-        if(turn === 1) return;
+        if(turn === 1 || endOfRound>0) return;
+        setTurn(1);
         //setTimeout(null,1000);
         setStatusBar("Fighter uses [MOVE 1]");
         await sleep(1200);
@@ -37,25 +38,27 @@ export default function Battle(props)
         console.log(`current enemy hp ${enemyHP}`);
         //int damage = move.getDamage()
         //setEnemyHP()
-        handleEndofRound();
-        setTurn(1);
-        enemyAttack();
+        await handleEndofRound();
+        await enemyAttack();
     }
     async function handleAttack2()
     {
-        if(turn === 1) return;
+        if(turn === 1 || endOfRound >0) return;
+        setTurn(1);
         console.log('handle attack 2 called');
         setStatusBar("Fighter uses [MOVE 2]");
         await sleep(1200);
-        // set enemy hp
+        setEnemyHP(enemyHP-50);
         await sleep(800);
-        setStatusBar("Enemy takes [DMG] damage!");
+        setStatusBar("Enemy takes 50 damage!");
         await sleep(2200);
-        handleEndofRound();
+        await handleEndofRound();
+        await enemyAttack();
     }
     async function handleAttack3()
     {
-        if(turn === 1) return;
+        if(turn === 1 || endOfRound >0) return;
+        setTurn(1);
         console.log('handle attack 3 called');
         setStatusBar("Fighter uses [MOVE 3]");
         await sleep(1200);
@@ -63,11 +66,13 @@ export default function Battle(props)
         await sleep(800);
         setStatusBar("Enemy takes [DMG] damage!");
         await sleep(2200);
-        handleEndofRound();
+        await handleEndofRound();
+        await enemyAttack();
     }
     async function handleAttack4()
     {
-        if(turn === 1) return;
+        if(turn === 1 || endOfRound >0) return;
+        setTurn(1);
         console.log('handle attack 4 called');
         setStatusBar("Fighter uses [MOVE 4]");
         await sleep(1200);
@@ -75,10 +80,11 @@ export default function Battle(props)
         await sleep(800);
         setStatusBar("Enemy takes [DMG] damage!");
         await sleep(2200);
-        handleEndofRound();
+        await handleEndofRound();
+        await enemyAttack();
     }
-    async function enemyAttack()
-    {
+    async function enemyAttack() {
+        if (endOfRound > 0) return;
         setStatusBar("ENEMY TURN");
         console.log('enemy attack called');
         await sleep(2200);
@@ -93,11 +99,37 @@ export default function Battle(props)
         setStatusBar(`Fighter takes ${dmg} damage!`);
         await sleep(2500)
         //setTimeout(null,500);
-        handleEndofRound();
-        setTurn(0);
-        setStatusBar("Player Turn!");
+        await handleEndofRound();
+        if (endOfRound === 0)
+        {
+            setTurn(0);
+            setStatusBar("Player Turn!");
+        }
+
     }
-    const handleEndofRound = () =>
+    async function handleEndofRound()
+    {
+        setEndOfRound(3);
+        console.log(`handle end of round called`);
+        console.log(`enemy hp ${enemyHP}`);
+        console.log(enemyHP <= 0);
+        if(fighterHP <=0)
+        {
+            setEndOfRound(2);
+            setStatusBar("YOUR PARTY HAS BEEN DEFEATED!");
+            await sleep(2500)
+        }
+        else if(enemyHP <= 0)
+        {
+            console.log(`enemy should be dead`);
+            setEndOfRound(1);
+            setStatusBar("YOU WON!");
+            await sleep(2500)
+        }
+        else setEndOfRound(0);
+
+    }
+    /*const handleEndofRound = () =>
     {
         console.log('handle end of turn is called');
         if(fighterHP <=0)
@@ -123,8 +155,8 @@ export default function Battle(props)
         else if(turn === 1)
         {
             setTurn(0);
-        }*/
-    }
+        }
+    }*/
     const spriteData = {
         w: 160,
         h: 200
