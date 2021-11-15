d967172e288e030aad0a7626e4ba6a1e66b6e3c9
ryanmcalpine <57776028+ryanmcalpine@users.noreply.github.com>
2021-11-15 01:35:36 -0800

    Added to & Rearranged Buttons

--- dcui/src/Components/Battle.js
+++ dcui/src/Components/Battle.js
@@ -247,30 +247,30 @@ export default function Battle(props)
                 </div>
                 <div style={{
                     position: 'absolute',
-                    top: 550,
-                    left:520
+                    top: 580,
+                    left: 570
                 }}>
                     {statusBar}
                 </div>
                 <div style={{
                     position: 'absolute',
-                    top: 550,
-                    left:320
+                    top: 580,
+                    left: 320
                 }}>
                     fighterHP: {fighterHP}
                 </div>
                 <div style={{
                     position: 'absolute',
-                    top: 550,
-                    left:820
+                    top: 580,
+                    left: 780
                 }}>
                     enemyHP: {enemyHP}
                 </div>
 
                 <div style={{
                     position: 'absolute',
-                    top: 600,
-                    left:180
+                    top: 620,
+                    left:300
                 }}>
                     {hasUsedMove ? (
                         <Button onClick={handleAttack1} variant="outlined" disabled>Attack 1</Button>
@@ -300,10 +300,24 @@ export default function Battle(props)
                 </div>
                 <div style={{
                     position: 'absolute',
-                    top: 600,
-                    left:380
+                    top: 620,
+                    left: 560
                 }}>
                     <Button onClick={endTurn} variant="outlined">End Turn</Button>
+                </div>
+                <div style={{
+                    position: 'absolute',
+                    top: 620,
+                    left: 740
+                }}>
+                    <Button onClick={endTurn} variant="outlined">Inventory</Button>
+                </div>
+                <div style={{
+                    position: 'absolute',
+                    top: 657,
+                    left: 740
+                }}>
+                    <Button onClick={endTurn} variant="outlined">Party Members</Button>
                 </div>
             </div>
         }
