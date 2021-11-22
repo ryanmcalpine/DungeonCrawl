c0097442999aba5baa5b1eb598edf20534d83574
ryanmcalpine <57776028+ryanmcalpine@users.noreply.github.com>
2021-11-22 02:34:11 -0800

    HighScores UI Component

---
+++ dcui/src/Components/HighScores.js
@@ -0,0 +1,77 @@
+import React, {useState, useEffect, Fragment} from 'react';
+import {Typography, Paper} from "@mui/material";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+import API from '../API_Interface/API_Interface';
+
+const usersTableAttributes = [
+    {
+        attributeName: 'User Name',
+        attributeDBName: 'userName',
+        align: 'left'
+    },
+    {
+        attributeName: 'High Score',
+        attributeDBName: 'highScore',
+        align: 'left'
+    }
+];
+
+let keyID = 0;
+const nextKey = () => keyID++;
+export default function HighScores(props)
+{
+    const [users, setUsers] = useState([]);
+    console.log(`HighScores table contains the following accounts: ${JSON.stringify(users)}`);
+
+    useEffect( () => {
+        const api = new API();
+
+        async function getUsers() {
+            console.log(`HighScores.js::HighScores()::useEffect(): getUsers() called`);
+            const usersJSONString = await api.allUsers();
+            console.log(`Users from the database: ${JSON.stringify(usersJSONString)}`);
+            setUsers(usersJSONString.data);
+        }
+
+        getUsers();
+    }, [])
+
+    return <Fragment>
+        {
+            users.length > 0 &&
+            <Typography component="div" variant='h3'>
+                <TableContainer component={Paper}>
+                    <Table sx={{minWidth: 650}} aria-label="High Scores table">
+                        <TableHead>
+                            <TableRow key={nextKey()}>
+                                {
+                                    usersTableAttributes.map(attr => <TableCell key={nextKey()}
+                                                                                   align={attr.align}>{attr.attributeName}</TableCell>)
+                                }
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {users.map((user) => (
+                                <TableRow
+                                    key={nextKey()}
+                                    sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                                >
+                                    {
+                                        usersTableAttributes.map(attr => <TableCell key={nextKey()}
+                                                                                       align={attr.align}>{user[attr.attributeDBName]}</TableCell>)
+                                    }
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Typography>
+        }
+    </Fragment>
+}
--- dcui/src/Menu/MainMenu.js
+++ dcui/src/Menu/MainMenu.js
@@ -3,11 +3,13 @@ import Button from '@mui/material/Button';
 import Dungeon from '../Components/Dungeon.js';
 import Battle from '../Components/Battle';
 import Armory from '../Components/Armory';
+import HighScores from '../Components/HighScores';
 
 export default function MainMenu({}){
     const [showMenu, setShowMenu] = React.useState(true);
     const [showBattle, setShowBattle] = React.useState(false);
     const [showArmory, setShowArmory] = React.useState(false);
+    const [showHighScores, setShowHighScores] = React.useState(false);
 
     const handleStartGame = () => {
         setShowMenu(false);
@@ -17,10 +19,15 @@ export default function MainMenu({}){
         setShowMenu(false);
         setShowArmory(true);
     }
+    const handleShowHighScores = () => {
+        setShowMenu(false);
+        setShowHighScores(true);
+    }
     const handleReturnToMenu = () => {
         setShowMenu(true);
         setShowBattle(false);
         setShowArmory(false);
+        setShowHighScores(false);
     }
 
     if( showMenu === true )
@@ -32,6 +39,8 @@ export default function MainMenu({}){
                 <Button variant={"outlined"} onClick={handleStartGame}>Start Game</Button>
                 <div/>
                 <Button variant={"outlined"} onClick={handleShowArmory}>Armory</Button>
+                <div/>
+                <Button variant={"outlined"} onClick={handleShowHighScores}>High Scores</Button>
             </div>
         )
     }
@@ -50,6 +59,15 @@ export default function MainMenu({}){
             <div>
                 <Button variant={"outlined"} onClick={handleReturnToMenu}>Return to Menu</Button>
                 <Armory/>
+            </div>
+        )
+    }
+    else if( showHighScores === true )
+    {
+        return(
+            <div>
+                <Button variant={"outlined"} onClick={handleReturnToMenu}>Return to Menu</Button>
+                <HighScores/>
             </div>
         )
     }
