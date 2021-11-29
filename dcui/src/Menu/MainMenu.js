d5a60f8845c9b03cfc13c5514c5b057397b5ffda
ryanmcalpine <57776028+ryanmcalpine@users.noreply.github.com>
2021-11-29 04:28:52 -0800

    High Scores table reposition

--- dcui/src/Menu/MainMenu.js
+++ dcui/src/Menu/MainMenu.js
@@ -112,7 +112,7 @@ export default function MainMenu({user, logoutAction}){
             <div style={{position: 'absolute', left: '3%', top: '2%'}}>
                 <Button variant={"outlined"} style={{backgroundColor:'#E1ECF7', color:"black"}} onClick={handleReturnToMenu}>Return to Menu</Button>
                 <span style={{position:'absolute', left:'42%', color:'#E1ECF7'}}><h1>DUNGEON CRAWL</h1></span>
-                <div style={{position: 'absolute', left: '2%', top: '3%'}}>
+                <div style={{position: 'relative', left: '2%', top: '3%'}}>
                     <HighScores/>
                 </div>
             </div>
